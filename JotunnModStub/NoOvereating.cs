c7111f589f68127c0cce854a1de21166d447fd6f
Spacetornado <tonedef1@gmail.com>
2022-12-19 23:19:11 -0800

    wef

--- JotunnModStub/NoOvereating.cs
+++ JotunnModStub/NoOvereating.cs
@@ -1,22 +1,22 @@
-﻿// JotunnModStub
-// a Valheim mod skeleton using Jötunn
+﻿// NoOvereating
+// a Valheim mod that adds versions of foods that last 2 hours each instead of however long they normally last. The new foods/recipes are otherwise identical to the original.
 // 
-// File:    JotunnModStub.cs
-// Project: JotunnModStub
+// File:    NoOvereating.cs
+// Project: NoOvereating
 
 using BepInEx;
 using Jotunn.Entities;
 using Jotunn.Managers;
 
-namespace JotunnModStub
+namespace NoOvereating
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
-    internal class JotunnModStub : BaseUnityPlugin
+    internal class NoOvereating : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.jotunnmodstub";
-        public const string PluginName = "JotunnModStub";
+        public const string PluginGUID = "com.spacetornado.noovereating";
+        public const string PluginName = "NoOvereating";
         public const string PluginVersion = "0.0.1";
         
         // Use this class to add your own localization to the game
@@ -26,10 +26,8 @@ namespace JotunnModStub
         private void Awake()
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("ModStub has landed");
+            Jotunn.Logger.LogInfo("NoOvereating mod loaded.");
             
-            // To learn more about Jotunn's features, go to
-            // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
         }
     }
 }
